a63a18e0de746e1b0848c65b2ca887c1f34f24f9
MrCreating <mrcreating2002@gmail.com>
2025-12-23 23:34:22 +0300

    context gen 1

---
+++ modules/context_generator.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import math
+import random
+from typing import Any, Dict, List, Optional, Tuple
+
+from modules import PairwiseMatrixGenerator
+
+class ContextGenerator:
+    """
+    Генератор синтетических контекстов (JSON-структуры) для экспериментов AEM-COM
+    """
+    pass
